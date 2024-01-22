44d227dc116cc7bd2fed501a47aebb7fcdb65061
maks <maks@maksbook.local>
2024-01-22 23:16:10 +0100

    changes

--- rekrutacjaB/database.py
+++ rekrutacjaB/database.py
@@ -1,8 +1,12 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base, relationship
+import os
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
-DB_URL = 'postgresql://maks@localhost:5432/onwelotask'
+load_dotenv()
 
+DB_URL = 'postgresql://{0}:{1}@localhost:5432/{2}'.format(os.getenv("USER"), os.getenv("PASS"), os.getenv("DB"))
+print(DB_URL)
 engine = create_engine(DB_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)

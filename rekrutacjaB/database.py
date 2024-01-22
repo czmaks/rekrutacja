8514fa14c9343505116daa517217c5cd52069beb
maks <maks@maksbook.local>
2024-01-22 23:23:14 +0100

    final

--- rekrutacjaB/database.py
+++ rekrutacjaB/database.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DB_URL = 'postgresql://{0}:{1}@localhost:5432/{2}'.format(os.getenv("USER"), os.getenv("PASS"), os.getenv("DB"))
-print(DB_URL)
+
 engine = create_engine(DB_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
